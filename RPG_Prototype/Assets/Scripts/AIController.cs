0f0e54ccefaf328bfe7fb3fd127811289f369c29
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-14 21:45:27 -0400

    Messing with AI

--- RPG_Prototype/Assets/Scripts/AIController.cs
+++ RPG_Prototype/Assets/Scripts/AIController.cs
@@ -10,18 +10,26 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseRadius = 7f;
+        [SerializeField] float suspicionTime = 3f;
+        [SerializeField] WaypointPatrol waypointPatrol;
+        [SerializeField] float waypointTolerance = 1.5f;
 
         Health health;      
         Fighter fighter;
         Mover mover;
+
         GameObject player;
+        float timeSinceSeenPlayer = Mathf.Infinity;
         Vector3 defaultPosition;
+        int currentWaypointIndex = 0;
+
 
         private void Start()
         {
             health = GetComponent<Health>();
             fighter = GetComponent<Fighter>();
             mover = GetComponent<Mover>();
+
             player = GameObject.FindWithTag("Player");
             defaultPosition = transform.position;
         }
@@ -33,18 +41,37 @@ namespace RPG.Control
             if (InAttackRange() && fighter.CanAttack(player))
             {
                 Debug.Log(this.name + " chasing player!");
+                timeSinceSeenPlayer = 0;
                 fighter.Attack(player);
+            }
+            else if (timeSinceSeenPlayer <= suspicionTime)
+            {
+                GetComponent<ActionScheduler>().CancelCurrentAction();
             }
             else
             {
-                mover.StartMoveAction(defaultPosition);               
+                //mover.StartMoveAction(defaultPosition);   
+                Patrol();
             }
+            timeSinceSeenPlayer += Time.deltaTime;
         }
 
         private bool InAttackRange()
         {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             return distanceToPlayer <= chaseRadius;
+        }
+
+        private void Patrol()
+        {
+            if (waypointPatrol != null)
+            {
+                float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+                if (distanceToWaypoint <= waypointTolerance)
+                {
+
+                }
+            }
         }
 
         private void OnDrawGizmosSelected()
---
+++ RPG_Prototype/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control 
+{
+    public class WaypointPatrol : MonoBehaviour
+    {
+        const float gizmoRadius = .3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int nextIndex = i + 1;
+                if (nextIndex >= transform.childCount)
+                {
+                    nextIndex = 0;
+                }
+
+                Gizmos.DrawSphere(transform.GetChild(i).position, gizmoRadius);
+                Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(nextIndex).position);
+            }
+        }
+
+    }
+
+}
+
+
