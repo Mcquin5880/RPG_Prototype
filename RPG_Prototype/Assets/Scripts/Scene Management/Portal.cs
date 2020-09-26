5c436af578d209efb3f576d7c2a75d157dc6ce35
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-25 21:44:42 -0400

    Scene management

--- RPG_Prototype/Assets/Scripts/Scene Management/Portal.cs
+++ RPG_Prototype/Assets/Scripts/Scene Management/Portal.cs
@@ -8,13 +8,46 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneIndexToLoad = -1;
+        [SerializeField] Transform playerSpawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
-                SceneManager.LoadScene(sceneIndexToLoad);
+                StartCoroutine(SceneTransition());
             }
+        }
+
+        private IEnumerator SceneTransition()
+        {
+            DontDestroyOnLoad(this.gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneIndexToLoad);
+
+            Portal exitPortal = GetExitPortal();
+            SpawnPlayerAtExitPortal(exitPortal);
+
+
+
+            Destroy(gameObject);
+        }
+
+        private Portal GetExitPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void SpawnPlayerAtExitPortal(Portal exitPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.transform.position = exitPortal.playerSpawnPoint.position;
+            player.transform.rotation = exitPortal.playerSpawnPoint.rotation;
         }
     }
 }
