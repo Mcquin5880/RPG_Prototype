bfc197ea4fba917443982d53cd854ee558f98ee7
NewbieMcCoder <Mquinter1988@gmail.com>
2020-10-29 19:39:06 -0400

    asdf

--- RPG_Prototype/Assets/Scripts/Stats/BaseStats.cs
+++ RPG_Prototype/Assets/Scripts/Stats/BaseStats.cs
@@ -8,6 +8,7 @@ namespace RPG.Stats
     {
         [Range(1, 99)] [SerializeField] int startingLevel = 1;
         [SerializeField] CharacterClass characterClass;
+        [SerializeField] Progression progression = null;
 
 
         // Start is called before the first frame update
---
+++ RPG_Prototype/Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+public class Progression : ScriptableObject
+{
+    [System.Serializable]
+    class ProgressionCharacterClass
+    {
+
+    }
+
+
+}
+
+
+
